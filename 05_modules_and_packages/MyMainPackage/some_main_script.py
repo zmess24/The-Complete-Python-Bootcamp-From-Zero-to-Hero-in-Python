d4eb167eac5fc1e68acefc884d88955279a08536
Zac Messinger <zdmessinger@gmail.com>
2023-05-28 16:12:49 -0700

    Updated Moudle

---
+++ 05_modules_and_packages/MyMainPackage/some_main_script.py
@@ -0,0 +1,2 @@
+def report_main():
+    print("Hey, I'm a package in the main script")
---
+++ 05_modules_and_packages/my_program.py
@@ -0,0 +1,8 @@
+# from my_module import my_func
+# my_func()
+
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import my_subscript
+
+some_main_script.report_main()
+my_subscript.sub_report()
