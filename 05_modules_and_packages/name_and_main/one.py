df273b5e95699ccefe642560e1fd43b7c871fc3f
Zac Messinger <zdmessinger@gmail.com>
2023-05-28 16:21:54 -0700

    finished module

---
+++ 05_modules_and_packages/name_and_main/one.py
@@ -0,0 +1,18 @@
+def func():
+    print("FUNC() IN ONE.PY")
+
+def func1():
+    print("THIS IS A FUNCTION")
+
+def func2():
+    print("This is a function!")
+print("TOP LEVEL IN ONE.PY")
+
+# if __name__ == "__main__":
+#     print("ONE.PY is being run directly!")
+# else:
+#     print("ONE.PY has been imported!")
+
+if __name__ == "__main__":
+    func1()
+    func2()
