f2d0619d0e87afc41aff396d02da8efbc779da01
Zac Messinger <zdmessinger@gmail.com>
2023-05-28 17:03:53 -0700

    updated

--- 05_modules_and_packages/name_and_main/two.py
+++ 05_modules_and_packages/name_and_main/two.py
@@ -8,3 +8,5 @@ if __name__ == "__main__":
     print("TWO.PY is being run directly!")
 else:
     print("TWO.PY has been imported!")
+
+
---
+++ 07_pylint/simple_one.py
@@ -0,0 +1,15 @@
+'''
+A Best Practice is to leave comments for other
+developers at the top.
+'''
+
+def myfunc():
+    '''
+    Adds two numbers together
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
